3de9886e0f1e0516fa00155243ff7cf16af80032
abdulalilm1010 <abdulalim243457@gmail.com>
2025-02-25 14:53:44 +0600

    again time

--- java/pay.js
+++ java/pay.js
@@ -3,9 +3,10 @@ document.getElementById('login-btn').addEventListener('click', function (event) 
   const acounNumber = document.getElementById('acount-number').value;
   
   const pin = document.getElementById('pin').value;
+  const convertedPin=parseInt(pin)
   if (acounNumber.length === 11) {
-    if (pin ===" 1234") {
-      console.log('okkk')
+    if (convertedPin === 1234) {
+      window.location.href="./main.html"
     }
     else {
       console.log('pin is wrong')
